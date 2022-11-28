8e3bbff1b83457dc56467bf1a8a645f3fb130974
jeevan kumar <jjjeeevvvaaan@gmail.com>
2022-11-28 17:46:37 +0530

    Designed a bus.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { React } from 'react';
 import './App.scss';
+import Bus from './components/Bus';
 
 const App = () =>
 	<div className="App" role="App">
-		Ready to start.
+		<Bus/>
 	</div>;
 
 export default App;
---
+++ src/components/Bus.js
@@ -0,0 +1,20 @@
+import { React } from 'react';
+
+const Bus = () => <div>
+	<div className="busBody"/>
+	<div className="frontWheel"/>
+	<div className="frontWheelInterior"/>
+	<div className="backWheel"/>
+	<div className="backWheelInterior"/>
+	<div className="frontDoor"/>
+	<div className="backDoor"/>
+	<div className="frontDoorGlass1"/>
+	<div className="frontDoorGlass2"/>
+	<div className="backDoorGlass1"/>
+	<div className="backDoorGlass2"/>
+	<div className="redLights"/>
+	<div className="frontGlass"/>
+	<div className="frontSideMirror"/>
+</div>;
+
+export default Bus;
